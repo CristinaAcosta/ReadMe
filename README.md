803f6a9c031dc08c30d6237cafccbe3deebb2adb
Cristina Acosta <cacost5695@yahoo.com>
2021-02-22 18:37:06 -0800

    nodess

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ReadMe
+Title: (NPM displays info)
+
+Description: (NPM displays info)
+
+Table of Contents: (linked table of contents )
+
+Installation: (NPM displays info)
+
+Usage: (NPM displays info)
+
+License: (NPM displays info and adds badge to corner)
+
+Contributing:(NPM displays info)
+
+Tests: (NPM displays info)
+
+Questions: (Adds Github Username & adds email w/ additonal questions on how to reach me )
---
+++ index.js
@@ -0,0 +1,76 @@
+const inq = require('inquirer');
+const fs = require('fs');
+var text = "";
+const quest = [{
+      message: 'Enter project tile.',
+      name: 'title',
+      type: 'input',
+  },
+  {
+    message: 'Enter project description.',
+    name: 'description',
+    type: 'input',
+},
+{
+    message: 'Enter project installation instructions.',
+    name: 'install',
+    type: 'input',
+},
+{
+    message: 'Enter project usage information.',
+    name: 'usage',
+    type: 'input',
+},
+{
+    message: 'Enter project contribution guidelines.',
+    name: 'contr',
+    type: 'input',
+},
+{
+    message: 'Enter project test instructions',
+    name: 'instruct',
+    type: 'input',
+},
+{
+message: 'Enter project license.',
+    name: 'license',
+    type: 'input',
+},
+{
+message: 'Enter Github Username',
+    name: 'user',
+    type: 'input',
+},
+{
+ message: 'Enter email address',
+    name: 'email',
+    type: 'input',
+ },
+
+];
+inq.prompt(quest).then( ans => {
+    text += `# ReadMe
+    Title: ${ans.title}
+    
+    Description: ${ans.description}
+    
+    Table of Contents: 
+    
+    Installation: ${ans.install}
+    
+    Usage: ${ans.usage}
+    
+    License: ${ans.license}
+    
+    Contributing: ${ans.contr}
+    
+    Tests: ${ans.instruct}
+    
+    Questions: ${ans.user} ${ans.email}
+
+    `;
+    fs.writeFile("README.md", text, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+});
